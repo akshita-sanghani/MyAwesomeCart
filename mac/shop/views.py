c751bfc6c873d24bf96471502ecb25c81c32d411
akshita sanghani <akshitasanghani8866@gmail.com>
2025-03-07 18:34:36 +0530

    updated some files and added new features

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -19,10 +19,34 @@ def index(request):
     params = {'allProds': allProds}
     return render(request, "shop/index.html", params)
 
+def searchMatch(query, item):
+    if query in item.Product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+
+def search(request):
+    query= request.GET.get('search')
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod=[item for item in prodtemp if searchMatch(query, item)]
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        if len(prod)!= 0:
+            allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds, "msg":""}
+    if len(allProds)==0 or len(query)<4:
+        params={'msg':"Please make sure to enter relevant search query"}
+    return render(request, 'shop/search.html', params)
+
 def about(request):
     return render(request, "shop/about.html")
 
 def contact(request):
+    thank = False
     if request.method == "POST":
         name = request.POST.get('name', '')
         email = request.POST.get('email', '')
@@ -45,13 +69,14 @@ def tracker(request):
                 updates = []
                 for item in update:
                     updates.append({'text': item.update_desc, 'time': item.timestamp})
-                    response = json.dumps([updates, order[0].items_json], default=str)
-
-                return HttpResponse(response)
+                    response = json.dumps({"status": "success", "updates": updates, "itemsJson": order[0].items_json},
+                                          default=str)
+                    return HttpResponse(response)
             else:
-                return HttpResponse('{}')
-        except Exception as e:
-            return HttpResponse('{}')
+                return HttpResponse('{"status":"noitem"}')
+
+        except:
+            return HttpResponse('{"status":"error"}')
 
     return render(request, 'shop/tracker.html')
 
